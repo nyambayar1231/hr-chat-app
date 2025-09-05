7c7cea6dec6eae5a72e9f7801b35cdef9cab46b4
new_user <new_user@new-users-MacBook-Pro.local>
2025-09-05 15:20:42 +0800

    Login check

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,11 @@
 'use client';
 
-import { signIn, useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { auth } from '@/auth';
 
 export default function LoginPage() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  console.log({ session });
-
-  useEffect(() => {
-    if (status === 'authenticated' && session?.user) {
-      router.push('/');
-    }
-  }, [session, status, router]);
-
-  // if (status === 'loading') {
-  //   return (
-  //     <div className="min-h-screen flex items-center justify-center p-6">
-  //       <div className="w-full max-w-sm space-y-6 text-center">
-  //         <p>Loading...</p>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  // const session = await auth();
 
   return (
     <div className="min-h-screen flex items-center justify-center p-6">
