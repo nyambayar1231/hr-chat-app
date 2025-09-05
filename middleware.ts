8051c4d3aaf14e71474350f81688c045d085cc18
new_user <new_user@new-users-MacBook-Pro.local>
2025-09-05 11:55:21 +0800

    check middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
 // export { auth as middleware } from '@/auth';
 
-// export const config = {
-//   matcher: [
-//     '/', // Homepage
-//     '/chat/:path*', // Chat routes
-//     '/api/chat', // Chat API
-//   ],
-// };
+export const middleware = (...res: any) => {
+  console.log(res);
+};
+
+export const config = {
+  matcher: [
+    '/', // Homepage
+    '/chat/:path*', // Chat routes
+    '/api/chat', // Chat API
+  ],
+};
