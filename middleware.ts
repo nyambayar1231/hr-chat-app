ee8b8ed4b8472743b3cbeb716d29048ca0dd72c0
new_user <new_user@new-users-MacBook-Pro.local>
2025-09-05 16:04:26 +0800

    middleware"

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 export { auth as middleware } from '@/auth';
 
+// export const config = {
+//   matcher: ['/chat', '/((?!api/auth|_next/|favicon.ico|login|public/).*)'],
+// };
+
 export const config = {
-  matcher: ['/chat', '/((?!api/auth|_next/|favicon.ico|login|public/).*)'],
+  matcher: ['/chat'],
 };
